568da571a2b21a62e6e9c8a09718696d9e156604
Alexander Hoang <Aleexx.hoang@gmail.com>
2023-01-04 17:02:20 +0100

    work on size method next

--- linkedList.rb
+++ linkedList.rb
@@ -1,18 +1,46 @@
 class LinkedList
 
+    attr_accessor :tail, :head
+    def initialize
+        @head = nil
+        @tail = nil
+    end
+
     def append(value)
-        @next_node = Node.new(value, nil)
+        x = Node.new(value, nil)
+        if @head == nil
+            @head = x
+            @tail = x
+        end
+        @tail.next_node = x
+        @tail = x
+
     end
 
     def prepend(value)
-        
+        if @head == nil
+            @head = Node.new(value, nil)
+            return
+        end
+        x = Node.new(value, @head)
+        @head = x
+    end
+
+    #-----------TODO----------
+    def size
+    
+    end
+    #-----------TODO----------
+    
+    def test
+        puts @head.next_node.inspect
+        puts @tail.inspect
     end
 
 end
 
-
 class Node
-    attr_accessor :value, :node 
+    attr_accessor :value, :next_node 
     
     def initialize(value=nil, next_node=nil)
         @value = value
@@ -22,4 +50,7 @@ class Node
 end
 
 my_list = LinkedList.new
-my_list.append(3)
+my_list.prepend(3)
+my_list.prepend(4)
+my_list.test
+my_list.prepend(7)
